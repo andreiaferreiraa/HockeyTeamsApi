6b2aa3272d441664268aeecc3f4923fa7d8105bf
andreiaferreiraa <aluno19985@ipt.pt>
2018-07-09 12:38:55 +0100

    Tratameno visual do div que contém os detalhes dos jogadores

--- HockeyTeamAPI/Js/logic.js
+++ HockeyTeamAPI/Js/logic.js
@@ -60,16 +60,18 @@ function ecraJogadores(id) {
 }
 
 function mostraJogadores(jogadores) {
-    //console.log(jogadores);
     //criação de elementos HTML
     var divJogador = document.createElement('div');
+    divJogador.setAttribute('class', 'divJogadores');
     var nomeJogador = document.createElement('p');
+    nomeJogador.setAttribute('class', 'NomeJogador');
     var numJogador = document.createElement('p');
     var posicaoJogador = document.createElement('p');
     var fotoJogador = document.createElement('img');
+    fotoJogador.setAttribute('class', 'fotoJogador1');
     //preencher o conteudo desses elementos
+    //numJogador.textContent = jogadores.Numero;
     nomeJogador.textContent = jogadores.Nome;
-    numJogador.textContent = jogadores.Numero;
     posicaoJogador.textContent = jogadores.Posicao;
     fotoJogador.src = "JogadoresFotosApi/" + jogadores.Fotografia;
     fotoJogador.addEventListener('click', function () {
@@ -77,10 +79,11 @@ function mostraJogadores(jogadores) {
     });
 
     //insersao dos elementos Html  dentro dos respetivos Div's
-    divJogador.appendChild(nomeJogador);
-    divJogador.appendChild(numJogador);
-    divJogador.appendChild(posicaoJogador);
+    
+    //divJogador.appendChild(numJogador);
+    //divJogador.appendChild(posicaoJogador);
     divJogador.appendChild(fotoJogador);
+    //divJogador.appendChild(nomeJogador);
     return divJogador;
 }
 
@@ -97,49 +100,147 @@ function mostraEcra2(id) {
 
 //*************************************************** DETALHES DOS JOGADORES *********************************************
 function mostraDetalhesJogador(detalhes) {
-    var divDetalhesJogadores = document.querySelector('#detalhes');
+    var divDetalhesJogadores = document.querySelector('#det');
     //criacao dos elementos HTML
     var divDetalhes = document.createElement('div');
-    //divDetalhes.setAttribute('class', 'detalhesJogador');
-    var nomeJogador = document.createElement('h5');
+    var divCadaDetalhe = document.createElement('div');
+    var paragrafoNome = document.createElement('h4');
+    var nomeJogador = document.createElement('h1');
+    nomeJogador.setAttribute('class', 'nomeJogador');
+    var barra = document.createElement('hr');
+    barra.setAttribute('class', 'barra');
+    //foto do jogador
     var fotoJogador = document.createElement('img');
-    fotoJogador.setAttribute('class', 'imagemJogador');
+    fotoJogador.setAttribute('class', 'fotoJogador2');
+    //nome completo do jogador
+    var paragrafoNome = document.createElement('h4');
     var nomeCompJogador = document.createElement('p');
+    //numero do jogador
+    var paragrafoNumero = document.createElement('h4');
     var numJogador = document.createElement('p');
+    //posicao
+    var paragrafoPosicao = document.createElement('h4');
     var posicao = document.createElement('p');
+    //data de nascimento
+    var paragrafoDataNasc = document.createElement('h4');
     var dataNasc = document.createElement('p');
+    //nacionalidade
+    var paragrafoNacionalidade = document.createElement('h4');
     var nacionalidade = document.createElement('p');
+    //altura
+    var paragrafoAltura = document.createElement('h4');
     var altura = document.createElement('p');
+    //peso
+    var paragrafoPeso = document.createElement('h4');
     var peso = document.createElement('p');
+    //*************************************************************//
     //preencher o conteudo desses elementos
-    nomeJogador.textContent = "Nome: " + detalhes.Nome;
+    nomeJogador.textContent = detalhes.Nome;
     fotoJogador.src = "JogadoresFotosApi/" + detalhes.Fotografia;
-    nomeCompJogador.textContent = "Nome Completo do Jogador: " + detalhes.NomeCompleto;
-    numJogador.textContent = "Número do Jogador: " + detalhes.Número;
-    posicao.textContent = "Posição: " + detalhes.Posicao;
-    dataNasc.textContent = "Data de Nascimento: " + detalhes.dataNasc;
-    nacionalidade.textContent = "Nacionalidade: " + detalhes.Nacionalidade;
-    altura.textContent = "Peso: " + detalhes.Altura;
-    peso.textContent = "Altura: " + detalhes.Peso;
+    //Nome Completo do Jogador
+    paragrafoNome.textContent = "NOME COMPLETO: ";
+    paragrafoNome.setAttribute('class', 'paragrafoNome');
+    nomeCompJogador.textContent = detalhes.NomeCompleto;
+    nomeCompJogador.setAttribute('class', 'classNomeComp');
+    //numero do jogador
+    paragrafoNumero.textContent = "NÚMERO: ";
+    paragrafoNumero.setAttribute('class', 'paragrafoNumero');
+    numJogador.textContent = detalhes.Número;
+    numJogador.setAttribute('class', 'numeroJogador');
+    //posicao
+    paragrafoPosicao.textContent = "POSIÇÃO: ";
+    paragrafoPosicao.setAttribute('class', 'paragrafoPosicao');
+    posicao.textContent = detalhes.Posicao;
+    posicao.setAttribute('class', 'posicao');
+    //data de nascimento
+    paragrafoDataNasc.textContent = "DATA DE NASCIMENTO: ";
+    paragrafoDataNasc.setAttribute('class', 'paragrafoDataNasc');
+    dataNasc.textContent = detalhes.dataNasc;
+    dataNasc.setAttribute('class', 'dataNascimento');
+    //nacionalidade
+    paragrafoNacionalidade.textContent = "NACIONALIDADE: ";
+    paragrafoNacionalidade.setAttribute('class', 'paragrafoNacionalidade');
+    nacionalidade.textContent = detalhes.Nacionalidade;
+    nacionalidade.setAttribute('class', 'nacionalidade');
+    //altura
+    paragrafoAltura.textContent = "ALTURA";
+    paragrafoAltura.setAttribute('class', 'paragrafoAltura');
+    altura.textContent = detalhes.Altura;
+    altura.setAttribute('class', 'altura');
+    //peso
+    paragrafoPeso.textContent = "PESO: ";
+    paragrafoPeso.setAttribute('class', 'paragrafoPeso');
+    peso.textContent = detalhes.Peso;
+    peso.setAttribute('class', 'peso');
+    //***********************************************************/
     //insersao dos elementos Html  dentro dos respetivos Div's
     divDetalhes.appendChild(nomeJogador);
+    divDetalhes.appendChild(barra);
     divDetalhes.appendChild(fotoJogador);
-    divDetalhes.appendChild(nomeCompJogador);
-    divDetalhes.appendChild(numJogador);
-    divDetalhes.appendChild(posicao);
-    divDetalhes.appendChild(dataNasc);
-    divDetalhes.appendChild(nacionalidade);
-    divDetalhes.appendChild(altura);
-    divDetalhes.appendChild(peso);
+
+    divCadaDetalhe.appendChild(paragrafoNome);
+    divCadaDetalhe.appendChild(nomeCompJogador);
+    divCadaDetalhe.appendChild(paragrafoNumero);
+    divCadaDetalhe.appendChild(numJogador);
+    divCadaDetalhe.appendChild(paragrafoPosicao);
+    divCadaDetalhe.appendChild(posicao);
+    divCadaDetalhe.appendChild(paragrafoDataNasc);
+    divCadaDetalhe.appendChild(dataNasc);
+    divCadaDetalhe.appendChild(paragrafoNacionalidade);
+    divCadaDetalhe.appendChild(nacionalidade);
+    divCadaDetalhe.appendChild(paragrafoAltura);
+    divCadaDetalhe.appendChild(altura);
+    divCadaDetalhe.appendChild(paragrafoPeso);
+    divCadaDetalhe.appendChild(peso);
+    divDetalhes.appendChild(divCadaDetalhe);
+    
     divDetalhesJogadores.appendChild(divDetalhes);
     getMultimedia(detalhes.ID).then(function (multimedia) {
-        var divInic = document.createElement('div');
-        for (var i = 0; i < multimedia.length; i++) {
-            var fotosJogador = document.createElement('img');
-            fotosJogador.src = "Multimédia/" + multimedia[i].NomeFotografia;
-            divInic.appendChild(fotosJogador);
+        //var divInic = document.createElement('div');
+        //div que é preciso para mostrar cada imagem
+        var divImagem = document.createElement('div');
+        divImagem.setAttribute('class', 'carousel-item active');
+        divImagem.setAttribute('id', 'divImagem');
+        //elemento que vai buscar cada imagem do jogador
+        var cadaImagem = document.createElement('img');
+        cadaImagem.setAttribute('class', 'd-block w-100');
+        cadaImagem.setAttribute('id', 'imagemJogador');
+        cadaImagem.setAttribute('src', "Multimédia/" + multimedia[0].NomeFotografia);
+        divImagem.appendChild(cadaImagem);
+        let im = document.querySelector('.carousel-inner');
+        im.appendChild(divImagem);
+        for (var i = 1; i <= multimedia.length-1; i++) {
+            //criação da lista de imagens para o "carrosel"
+            var listaImagens = document.createElement('li');
+            listaImagens.setAttribute('data-target', '#carouselExampleIndicators');
+            listaImagens.setAttribute('data-slide-to', i);
+            //atribuição ao carrossel a lista de imagens
+            var carousel = document.querySelector('.carousel-indicators');
+            carousel.appendChild(listaImagens);
+
+            //div que é preciso para mostrar cada imagem
+            var divImagem = document.createElement('div');
+            divImagem.setAttribute('class', 'carousel-item');
+            //elemento que vai buscar cada imagem do jogador
+            var cadaImagem = document.createElement('img');
+            cadaImagem.setAttribute('class', 'd-block w-100');
+            cadaImagem.setAttribute('id', 'imagemJogador');
+            cadaImagem.setAttribute('src', "Multimédia/" + multimedia[i].NomeFotografia);
+            divImagem.appendChild(cadaImagem);
+            let im = document.querySelector('.carousel-inner');
+            im.appendChild(divImagem);
+
+
+            //<div class="carousel-item active">
+            //    <img class="d-block w-100" src="...">
+            //    </div>
+
+
+            //var fotosJogador = document.createElement('img');
+            //fotosJogador.src = "Multimédia/" + multimedia[i].NomeFotografia;
+            //divInic.appendChild(fotosJogador);
         }
-        divDetalhesJogadores.appendChild(divInic);
+        //divDetalhesJogadores.appendChild(divInic);
     });
 }
 
